2710f201fea83a077cf2e508998907abb8fe1fa6
Ilsaf Khamidullin <justsairex@gmail.com>
2024-04-29 21:16:48 +0300

    rm artefact files

--- task_2_py_files/task_2_408601958.py
+++
@@ -1,6 +0,0 @@
-from math import log2
-from typing import List
-
-
-def entropy(class_probabilities: List[float]) -> float:
-    return sum(-p*log2(p) for p in class_probabilities if p > 0)
--- test_storage/task_test_123.py
+++
@@ -1,3 +0,0 @@
-
-def test_task(a):
-    return f"Hello! {a}"
